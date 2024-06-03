2b97e3f170600376e8948d0348f10f94cbdc9a64
shokhruzaibodova <shokhruzaibodova@gmail.com>
2024-06-03 17:50:12 +0500

    new commit

---
+++ apps/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as __
+from apps.models import Product, Category, StudentUser, TeacherUser, SubjectUser
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'price'
+    autocomplete_fields = 'category',
+
+
+@admin.register(StudentUser)
+class StudentUserModelAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "birth_date")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(type=StudentUser.Type.STUDENT)
+
+
+@admin.register(TeacherUser)
+class TeacherUserModelAdmin(UserAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(type=TeacherUser.Type.TEACHER)
+
+
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+
+
+@admin.register(SubjectUser)
+class SubjectsUserModelAdmin(UserAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(type=StudentUser.Type.SUBJECT)
---
+++ apps/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, DeleteView
+
+from apps.models import User
+
+
+class MainTemplateView(TemplateView):
+    template_name = 'apps/index.html'
+
+
+class StudentListView(ListView):
+    queryset = User.objects.filter(type=User.Type.STUDENT)
+    template_name = 'apps/student/list.html'
+    context_object_name = 'students'
+
+
+class StudentDetailView(DetailView):
+    queryset = User.objects.filter(type=User.Type.STUDENT)
+    template_name = 'apps/student/detail.html'
+    context_object_name = 'student'
+
+
+class StudentDeleteView(View):
+    def get(self, request, pk):
+        User.objects.filter(type=User.Type.STUDENT, id=pk).delete()
+        return redirect('student_list_page')
+
+
+class StudentUpdateView(UpdateView):
+    queryset = User.objects.filter(type=User.Type.STUDENT)
+    fields = 'first_name', 'last_name', 'email', 'birth_date', 'image'
+    template_name = 'apps/student/edit.html'
+    context_object_name = 'student'
+
+    def get_success_url(self):
+        return reverse('student_edit_page', kwargs={'pk': self.kwargs.get(self.pk_url_kwarg)})
+
+
+class SubjectListView(ListView):
+    queryset = User.objects.filter(type=User.Type.SUBJECT)
+    template_name = 'apps/subject/list.html'
+    context_object_name = 'subjects'
